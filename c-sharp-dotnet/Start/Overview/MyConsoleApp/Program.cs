f8040566f6d171610509b81a4340fa1550502d9e
sivasankarsani <shivasankar.sani@gmail.com>
2023-03-07 18:47:20 +0530

    #5 added initial dotnet concepts

---
+++ c-sharp-dotnet/Start/Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+string userName;
+
+Console.WriteLine("please provide your name here..!!");
+
+userName = Console.ReadLine();
+Console.WriteLine($"welcome to the dotnet course: {userName}");
+Console.WriteLine("This is the testing the app");
+
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
